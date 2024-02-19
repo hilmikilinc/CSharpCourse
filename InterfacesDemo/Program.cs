26e08b2152f753e580a1f5590b4c5ef7d781b0b5
Hilmi KILINÇ <hilmikilinc7@gmail.com>
2024-02-20 01:11:00 +0300

    CSharpCourse
    Commit

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            //foreach (var worker in workers) 
+            //{
+            //    worker.Work();
+            //}
+
+            IEat[] eats = new IEat[2]
+                {
+                  new Worker(),
+                  new Manager()
+                };
+            foreach (var eat in eats) 
+            {
+                eat.Eat();
+            }
+        }
+    }
+    //SOLID PRENSIPLERI
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+           
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Value Types
+       
+        decimal number6 = 10;
+        double number5 = 10.4; //64 bit
+        char character = 'A'; //ASCII
+        bool condition = false;
+        byte number4 = 255;
+        short number3 = 32767;
+        int number1 = 2147483647;
+        long number2 = 9223372036854775807;
+        var number7 = 10;
+        number7 = 'A';
+        //number7 = "A";
+
+        Console.WriteLine("number1 is {0}", number1);
+        Console.WriteLine("number2 is {0}", number2);
+        Console.WriteLine("number3 is {0}", number3);
+        Console.WriteLine("number4 is {0}", number4);
+        Console.WriteLine("Caharacter is : {0}", (int)character);
+        Console.WriteLine("number5 is {0}", number5);
+        Console.WriteLine("number7 is {0}", number7);
+        Console.WriteLine((int)Days.Friday);
+        Console.ReadLine();
+    }
+
+    enum Days
+    {
+        Monday=10, Tuesday=20, Wednesday=30, Thursday, Friday, Saturday, Sunday
+    }
+}
